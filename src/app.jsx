95ad1d02393d145119ec6c09bdede223639f8f38
User <marcio.b.a.dias@gmail.com>
2024-06-15 10:49:53 -0300

    ✅ Abrindo esboço do projeto

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <h1>Boilerplace + React</h1>
+      <h1>Quadro Matriz</h1>
     </>
   )
 }
---
+++ src/inicial.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    <>
+        <h1>Quadro de salas</h1>
+        <div className="container">
+            <div className="box">
+            <h2>São Pedro</h2>
+            </div>
+            <div className="box">
+            <h2>Matriz</h2>
+            </div>
+            <div className="box">
+            <h2>Passagem</h2>
+            </div>
+        </div>
+    </>
+}
+
+export { Home }
